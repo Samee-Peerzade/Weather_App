3290389fd5b8fefae95ec1c16e3fe42bcf61362b
Samee Peerzade <109617585+Samee-Peerzade@users.noreply.github.com>
2023-02-21 18:57:03 -0800

    Update app.js

--- Code/app.js
+++ Code/app.js
@@ -1,44 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>weather forecast</title>
-   
-</head>
-<body>
-    <style>
-        .botton {
-            
-            
-            background:black;
-            padding: 0.75em 2em 0.75em 2.5em;
-            color: white;
-            text-decoration: none;
-            margin: 0.5em 0 0;
-            border-radius: 35px;
-            border-color:blueviolet;
-            
-            
-            font-family: "Playfair Display",serif;
-            
-          
-          }
+const express = require("express");
+const https = require ("https");
+const bodyParser=require("body-parser")
 
-        .top{
-            background-color: rgb(164, 226, 133);
-            padding: 20px;
-            margin:20px
-        }
-    </style>
-<div class="top">
-    <form action="/" method="POST">
-        <h1>Please Enter City Name to know the Current Temperature</h1>
-        <label for="cityInput">City Name:</label>
-        <input id="cityInput" type="text" name="cityName">
-        <button class="botton" type="submit">Go</button>
-    </form>
-</div>    
-</body>
-</html>
+const app=express();
+
+
+app.use(bodyParser.urlencoded({extend:true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+
+});
+app.post("/", function(req,res){
+    
+    const query=req.body.cityName;
+    const apikey="API KEY"
+    const units="metric"
+
+
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apikey+"&units=" + units
+    https.get(url,function(response){
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            const weatherData=JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon=weatherData.weather[0].icon
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+
+            res.write("<p> The weather is Currently "+ weatherDescription +"</p>")
+            res.write("<h1> The temperature in "+ query + " is "+ temp + " Degrees celcius.</h1>")
+            res.write("<img src=" + imageURL + ">")
+            res.send()
+        })
+
+
+    })
+})
+
+app.listen(3000, function(){
+    console.log('Server is running on port 3000');
+})
